0c4b0b2ff30ef8f180dc862458e949bf7491c219
Ian Fleming <iantfleming1992@gmail.com>
2020-10-20 10:59:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,10 +44,8 @@ end
 def draw?(board)
   if !won(board) && full(board)
     return true
-  elsif won(board)
-    return false
-  else
-    return true
+  else  
+    return false 
   end    
 end
   
