26849bc7f2c23fd99bb0a342c5ead9a9fb83b234
Ian Fleming <iantfleming1992@gmail.com>
2020-10-20 11:14:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,7 +59,11 @@ def winner(board)
     if index == false
       return nil
     else
-      if board[index]
+      if board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
     end
   end
 end
